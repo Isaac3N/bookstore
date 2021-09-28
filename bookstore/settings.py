d8e4bf0317980be0c94ce74844426a79cca1e52b
isaac3n <theisaac3n@gmail.com>
2021-09-28 12:53:08 +0100

    initial user admin setup

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#auth user model
+AUTH_USER_MODEL = 'users.CustomUser'
